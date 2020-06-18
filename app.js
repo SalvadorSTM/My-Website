3f01d1ff38a646ab9e47f2a7b2a92ac72c8eae2d
SalvadorSTM <salvadort.martinez@gmail.com>
2020-06-17 23:07:37 -0400

    add auto detection of https vs http mode depending on development mode vs prod mode

--- app.js
+++ app.js
@@ -14,30 +14,42 @@ app.use(express.static(__dirname, {
 }));
 
 
-httpsport = "3001";
+httpsPort = "3001";
+httpPort = "3000";
 
-//Certificate
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/imsalvador.com/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/imsalvador.com/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/imsalvador.com/chain.pem', 'utf8');
+//Run SSL if windows not detected
+if (process.platform !== "win32") {
+  //Certificates to run with SSL
+  const privateKey = fs.readFileSync('/etc/letsencrypt/live/imsalvador.com/privkey.pem', 'utf8');
+  const certificate = fs.readFileSync('/etc/letsencrypt/live/imsalvador.com/cert.pem', 'utf8');
+  const ca = fs.readFileSync('/etc/letsencrypt/live/imsalvador.com/chain.pem', 'utf8');
 
-const credentials = {
-  key: privateKey,
-  cert: certificate,
-  ca: ca
-};
+  const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+  };
+  //Create HTTPS server
+  const httpsServer = https.createServer(credentials, app);
+
+  httpsServer.listen(httpsPort, function() {
+    console.log('HTTPS Server running on port 3001' + httpsPort + ' SSL mode enabled!');
+  });
+}
 
 
+//Run unsecure for development
+if (process.platform == "win32") {
+  // Start in http mode
+  const httpServer = http.createServer(app);
+  httpServer.listen(httpPort, function() {
+    console.log('HTTP Server running on port ' + httpPort + ' for windows NO SSL!');
+  });
+}
+
+
+//Direct users
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
-  
-})
 
-
-// Starting both http & https servers
-
-const httpsServer = https.createServer(credentials, app);
-
-httpsServer.listen(httpsport, function(){
-  console.log('HTTPS Server running on port 3001');
 });
