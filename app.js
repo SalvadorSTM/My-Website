64af2f192e6471b942f3d4653ceb49b695bfdb25
SalvadorSTM <salvadort.martinez@gmail.com>
2020-06-21 22:23:04 -0400

    Add google analytics, add section1 transitions, create common bootstrap container for sections

--- app.js
+++ app.js
@@ -16,35 +16,11 @@ app.use(express.static(__dirname, {
 httpsPort = "3001";
 httpPort = "3000";
 
-//Run with SSL if windows not detected
-if (process.platform !== "win32") {
-  //Certificates to run with SSL
-  const privateKey = fs.readFileSync('/etc/letsencrypt/live/imsalvador.com/privkey.pem', 'utf8');
-  const certificate = fs.readFileSync('/etc/letsencrypt/live/imsalvador.com/cert.pem', 'utf8');
-  const ca = fs.readFileSync('/etc/letsencrypt/live/imsalvador.com/chain.pem', 'utf8');
-
-  const credentials = {
-    key: privateKey,
-    cert: certificate,
-    ca: ca
-  };
-  //Create HTTPS server
-  const httpsServer = https.createServer(credentials, app);
-
-  httpsServer.listen(httpsPort, function() {
-    console.log('HTTPS Server running on port 3001' + httpsPort + ' SSL mode enabled!');
-  });
-}
-
-
-//Run unsecure for development
-if (process.platform == "win32") {
-  // Start in http mode
-  const httpServer = http.createServer(app);
-  httpServer.listen(httpPort, function() {
-    console.log('HTTP Server running on port ' + httpPort + ' for development NO SSL!');
-  });
-}
+// Start in http mode
+const httpServer = http.createServer(app);
+httpServer.listen(httpPort, function() {
+  console.log('HTTP Server running on port ' + httpPort);
+});
 
 
 //Direct users
